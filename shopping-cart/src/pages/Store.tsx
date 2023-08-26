1fc7265261dc44a1f58638debac7a96a16c1d2a5
Welcome ! <usmanubaid9@gmail.com>
2023-08-26 15:21:19 +0200

    feat: add styling

--- shopping-cart/src/pages/Store.tsx
+++ shopping-cart/src/pages/Store.tsx
@@ -1,7 +1,17 @@
 import { useState, useEffect } from "react";
+import { Row, Col, Card } from "react-bootstrap";
+
+type Product = {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  image: string;
+};
 
 const Store = () => {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState<Product[]>([]);
+
   const fetchData = async () => {
     try {
       const fetchData = await fetch("https://fakestoreapi.com/products");
@@ -14,17 +24,27 @@ const Store = () => {
 
   useEffect(() => {
     fetchData();
+    console.log(data);
   }, []);
 
   return (
     <>
-      <h1>Store</h1>
-
-      <div>
-        {data.map((item, index) => (
-          <p key={index}>{item.title}</p>
+      <h1 className="mt-4 ">Store</h1>
+      <Row xs={1} md={2} lg={4}>
+        {data?.map((item) => (
+          <Col key={item.id} className="g-3 mb-5">
+            <Card className="d-flex" style={{ height: "100%" }}>
+              <Card.Img variant="top" src={item.image} className="card-img" />
+              <Card.Body>
+                <Card.Title className="fs-6 d-flex justify-content-between align-items-baseline">
+                  <span className="w-75">{item.title}</span>
+                  <span>${item.price}</span>
+                </Card.Title>
+              </Card.Body>
+            </Card>
+          </Col>
         ))}
-      </div>
+      </Row>
     </>
   );
 };
