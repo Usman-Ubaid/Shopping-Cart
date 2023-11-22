f8b58be906e872e84f16183ed218233fb452cdfd
Welcome ! <usmanubaid9@gmail.com>
2023-11-22 22:46:36 +0100

    fix: remove redundant routes

--- shopping-cart/src/components/Navbar.tsx
+++ shopping-cart/src/components/Navbar.tsx
@@ -11,13 +11,7 @@ const Navbar = () => {
         <Container>
           <Nav>
             <Nav.Link to="/" as={NavLink}>
-              Home
-            </Nav.Link>
-            <Nav.Link to="/store" as={NavLink}>
               Store
-            </Nav.Link>
-            <Nav.Link to="/about" as={NavLink}>
-              About
             </Nav.Link>
           </Nav>
           {cartQuantity > 0 && (
--- shopping-cart/src/routes/AppRoutes.tsx
+++ shopping-cart/src/routes/AppRoutes.tsx
@@ -1,6 +1,4 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "../pages/Home";
-import About from "../pages/About";
 import Store from "../pages/Store";
 
 type Route = {
@@ -10,11 +8,7 @@ type Route = {
 };
 
 const AppRoutes = () => {
-  const routes: Route[] = [
-    { id: 1, page: <Home />, path: "/" },
-    { id: 2, page: <Store />, path: "/store" },
-    { id: 3, page: <About />, path: "/about" },
-  ];
+  const routes: Route[] = [{ id: 2, page: <Store />, path: "/" }];
 
   return (
     <Routes>
